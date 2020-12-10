8b55f47ae4745bfb2768151aa8400679f25f13e9
Trà Lê <chexanh1998@gmail.com>
2020-12-10 13:56:23 +0700

    Update return route after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -58,7 +58,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect()->route('admin.dashboard');
     }
 
     /**
