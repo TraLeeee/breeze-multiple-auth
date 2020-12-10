d20200614c4ef4b8612dfa540ae33833be26e0d5
Trà Lê <chexanh1998@gmail.com>
2020-12-10 13:29:57 +0700

    Add route group authenticated admin

--- routes/admin.php
+++ routes/admin.php
@@ -13,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => ['auth:admin']], function () {
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
