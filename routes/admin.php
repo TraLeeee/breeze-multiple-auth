882d0ec25e3896209646deca4434516798434ccf
Trà Lê <chexanh1998@gmail.com>
2020-12-10 16:18:12 +0700

    Add route logout admin

--- routes/admin.php
+++ routes/admin.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('logout', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'adminDestroy'])
+Route::post('logout', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'adminDestroy'])
     ->name('admin.logout');
 Route::group(['middleware' => ['auth:admin']], function () {
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
